34f7d870d2d6fc88b84e8da1cd6190b011079326
denizerkut04 <116192598+denizerkut04@users.noreply.github.com>
2024-08-23 00:10:23 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Quiz from "./components/quiz/Quiz";
+
+const App = () => {
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
+
+//imported quiz component from the quiz folder
